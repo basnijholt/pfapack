703ab4b5ec113ed3dcf2e92c88313d90633fa3bf
Bas Nijholt <basnijholt@gmail.com>
2020-03-12 16:24:58 +0100

    Bump version: 0.2.0 → 0.2.1.dev0

--- pfapack/__init__.py
+++ pfapack/__init__.py
@@ -1,2 +1,2 @@
 # pfapack Python package
-__version__ = "0.2.0"
+__version__ = "0.2.1.dev0"
--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ install_requires = ["scipy", "numpy"]
 setup(
     name="pfapack",
     python_requires=">=3.6",
-    version="0.2.0",
+    version="0.2.1.dev0",
     classifiers=[
         "Intended Audience :: Science/Research",
         "Operating System :: OS Independent",
