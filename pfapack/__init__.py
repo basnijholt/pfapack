920d1ef3696ed474bc835837fcb1055bc8b49f71
jjgoings <jjgoings@gmail.com>
2024-07-31 12:02:26 -0700

    change build

--- pfapack/__init__.py
+++
@@ -1,4 +0,0 @@
-# pfapack Python package
-from pfapack._version import __version__
-
-__all__ = ["__version__"]
--- setup.py
+++ setup.py
@@ -3,11 +3,9 @@
 import sys
 import os
 import subprocess
-import numpy
-from setuptools import find_packages, setup, Extension
+from setuptools import find_packages, setup, Command
+from setuptools.command.build_py import build_py
 from setuptools.command.build_ext import build_ext
-from setuptools.command.install import install
-from Cython.Build import cythonize
 
 if sys.version_info < (3, 9):
     print("pfapack requires Python 3.9 or above.")
@@ -16,74 +14,79 @@ if sys.version_info < (3, 9):
 with open("README.md") as f:
     readme = f.read()
 
-extras_require = dict(
-    docs=[
-        "sphinx",
-        "sphinx-rtd-theme",
-        "m2r",  # markdown support
-        "sphinxcontrib.apidoc",  # run sphinx-apidoc when building docs
-    ],
-    dev=["pre-commit"],
-    testing=["pytest", "pytest-cov", "pytest-mypy", "tox"],
-)
+extras_require = {
+    "docs": ["sphinx", "sphinx-rtd-theme", "m2r", "sphinxcontrib.apidoc"],
+    "dev": ["pre-commit"],
+    "testing": ["pytest", "pytest-cov", "pytest-mypy", "tox"],
+}
 
 install_requires = ["scipy", "numpy", "Cython", "setuptools"]
 
-def get_version_and_cmdclass(package_name):
-    from importlib.util import module_from_spec, spec_from_file_location
-
-    spec = spec_from_file_location("version", os.path.join(package_name, "_version.py"))
-    module = module_from_spec(spec)
-    spec.loader.exec_module(module)
-    return module.__version__, module.cmdclass
-
-version, cmdclass = get_version_and_cmdclass("pfapack")
-
-# Custom build command to compile the C and Fortran libraries
+# Custom command to ensure the C and Fortran libraries are built before the Python package
 class CustomBuildExtCommand(build_ext):
-    """Custom build command to compile the C library."""
     def run(self):
-        # Compile the Fortran library
+        print("Building external libraries...")
         subprocess.check_call(['make', '-C', 'external/fortran'])
-        # Compile the C interface library
         subprocess.check_call(['make', '-C', 'external/c_interface'])
+        lib_path = os.path.abspath("pfapack/libcpfapack.so")
+        print(f"Expected location of libcpfapack.so: {lib_path}")
+        if os.path.exists(lib_path):
+            print("libcpfapack.so found.")
+        else:
+            print("libcpfapack.so NOT found.")
         super().run()
 
-class CustomInstallCommand(install):
-    """Custom install command to run build_ext before install."""
+class CustomBuildPyCommand(build_py):
+    """Custom build command to ensure native libraries are built first."""
     def run(self):
         self.run_command('build_ext')
-        install.run(self)
+        super().run()
+
+class CustomCleanCommand(Command):
+    """Custom clean command to tidy up the project root."""
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        os.system('rm -rf ./build ./dist ./*.pyc ./*.tgz ./*.egg-info')
+        os.system('make clean -C external/c_interface')
+        os.system('make clean -C external/fortran')
 
 setup(
     name="pfapack",
     python_requires=">=3.9",
-    version="0.0.1",  # changed cause git tags were causing issues for me
-    cmdclass={'build_ext': CustomBuildExtCommand, 'install': CustomInstallCommand},
+    version="0.0.1",
+    cmdclass={
+        'build_ext': CustomBuildExtCommand,
+        'build_py': CustomBuildPyCommand,
+        'clean': CustomCleanCommand,
+    },
     classifiers=[
         "Intended Audience :: Science/Research",
         "Operating System :: OS Independent",
-        "Programming Language :: Python",
         "Programming Language :: Python :: 3",
         "Topic :: Scientific/Engineering",
-        "Topic :: Utilities",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3.9",
     ],
     description="Efficient numerical computation of the Pfaffian for dense and banded skew-symmetric matrices.",
     long_description=readme,
     long_description_content_type="text/markdown",
-    url="https://github.com/basnijholt/pfapack",
+    url="https://github.com/jjgoings/pfapack",
     author="Bas Nijholt (package) and M. Wimmer (code)",
     author_email="basnijholt@gmail.com",
     license="MIT",
     packages=find_packages(),
     package_data={
-        'pfapack': ['libcpfapack.so'],  # Ensure this path is correct
+        'pfapack': ['libcpfapack.so'],
     },
     include_package_data=True,
     install_requires=install_requires,
     extras_require=extras_require,
     zip_safe=False,
-    include_dirs=[numpy.get_include()],
 )
